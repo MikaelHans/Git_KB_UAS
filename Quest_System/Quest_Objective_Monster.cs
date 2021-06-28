b657e05c689e8f6e2f17a654f7935b0f259bad8d
MikaelHans <73513335+MikaelHans@users.noreply.github.com>
2021-06-29 01:49:43 +0700

    Update Quest_Objective_Monster.cs

--- Quest_System/Quest_Objective_Monster.cs
+++ Quest_System/Quest_Objective_Monster.cs
@@ -6,11 +6,14 @@ public class Quest_Objective_Monster : Quest_Objective
 {
     [SerializeField]
     Enemy _enemy;
+    [SerializeField]
+    int _slayed_count;
 
     public Quest_Objective_Monster(Enemy enemy, int count = 1)
     {
         this._enemy = enemy;
         this.count = count;
+        _slayed_count = 0;
     }
 
     public Enemy Enemy { get => _enemy; set => _enemy = value; }
